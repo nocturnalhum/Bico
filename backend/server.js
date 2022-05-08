bef51929900b81bfa36e190ba07d97a8892389c3
nocturnalhum <thanin.chan@gmail.com>
2022-05-08 04:52:40 -0400

    Integrate MongoDB database with Server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5050;
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Connect to MongoDB:
+const ConnectDB = require('./config/database');
+ConnectDB();
 
 // ============================================================================
 // =================<<< Middleware >>>=========================================
